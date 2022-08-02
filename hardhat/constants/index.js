4ca9c1eeeaafbde62816409541fbaeb2e5deae79
leon <leonhudak@protonmail.com>
2022-08-02 16:41:44 +0100

    added frontend

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x1c9865ECc227a67eD9712dE932353c26bD3cF9a3";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
